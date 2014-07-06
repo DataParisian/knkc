7464897f5ecc51d5ebcff5f6fda9e214c45fbdd0
DataParisian <sundeepkolli@gmail.com>
2014-07-06 03:09:01 +0200

    ch16 06-July-2014

---
+++ ch16/inventory/readline.c
@@ -0,0 +1,27 @@
+/* KNKC chapter 16 inventory.c
+ * Sundeep Kolli 05-july-2014
+ * Program: inventory.c
+ * Purpose: Maintains a parts database (arrays version).
+*/
+
+#include <ctype.h>
+#include <stdio.h>
+#include "readline.h"
+
+int read_line(char str[], int n){
+
+	int ch, i = 0;
+	
+	while (isspace(ch = getchar()))
+		;
+	
+	while (ch != '\n' && ch != EOF){
+		if (i < n)
+			str[i++] = ch;
+		ch = getchar();
+	}
+	str[i] = '\0';
+	
+	return i;
+}
+
